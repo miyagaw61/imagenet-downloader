e929a99d6b3c94885f5bfe89e69fa286f8076855
miyagaw61 <miyagaw61@gmail.com>
2018-09-04 17:50:08 +0900

    Add error handling to do_download

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs::*;
 use std::io::Read;
 use clap::{App, Arg};
 use regex::Regex;
+use std::thread::sleep;
 
 fn create_filename(dirname: &str, filename: &str) -> String {
     let result = [dirname, filename].join("/");
@@ -14,14 +15,31 @@ fn create_filename(dirname: &str, filename: &str) -> String {
     return result;
 }
 
-fn do_download(filename: &str, url: &str) {
+fn do_download(dirname: &str, filename: &str, url: &str) {
     if ! Path::new(filename).exists() {
-        let mut f = File::create(filename).unwrap();
-        println!("{}", ["Download: ", filename].join(""));
-        let mut res = reqwest::get(url).unwrap();
-        res.copy_to(&mut f).unwrap();
+        for i in 1..4 {
+            match reqwest::get(url) {
+                Ok(mut res) => {
+                    println!("{}", ["[+]Download: ", filename].join(""));
+                    if ! Path::new(dirname).exists() {
+                        create_dir(dirname).unwrap();
+                    }
+                    let mut f = File::create(filename).unwrap();
+                    res.copy_to(&mut f).unwrap();
+                    break;
+                },
+                Err(_e) => {
+                    println!("{}", ["[!]RETRY: ", filename].join(""));
+                    if i == 3 {
+                        println!("{}", ["[!]Skipped: ", filename, " (download error)"].join(""));
+                    }
+                    sleep(std::time::Duration::from_millis(2000));
+                    continue;
+                }
+            }
+        }
     } else {
-        println!("{}", ["Skipped: ", filename].join(""));
+        println!("{}", ["[!]Skipped: ", filename, " (already exists)"].join(""));
     }
 }
 
@@ -39,14 +57,20 @@ fn imagenet_download(url_list: &str) {
         let re = Regex::new(" +").unwrap();
         let id_url: Vec<&str> = re.split(line).collect();
         let id = id_url[0];
-        let url = id_url[1];
+        let url = if id_url.len() > 1 { id_url[1] } else { "" };
+        if url == "" {
+            println!("{}", ["[!]INCORRECT URL FORMAT: ", line].join(""));
+            continue;
+        }
         let dirname_filename: Vec<&str> = id.split("_").collect();
         let dirname = dirname_filename[0];
-        let filename = create_filename(dirname, dirname_filename[1]);
-        if ! Path::new(&dirname).exists() {
-            create_dir(&dirname).unwrap();
+        let filename = if dirname_filename.len() > 1 { dirname_filename[1] } else { "" };
+        if filename == "" {
+            println!("{}", ["[!]INCORRECT ID FORMAT: ", line].join(""));
+            continue;
         }
-        do_download(&filename, url);
+        let filename = create_filename(dirname, filename);
+        do_download(&dirname, &filename, url);
     }
 }
 
